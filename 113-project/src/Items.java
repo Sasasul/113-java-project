0c0d1410c4846050330504aa13dab61cda71f3a8
L0 <jana16.kahld@gmail.com>
2024-02-27 04:41:18 +0300

    test one

--- 113-project/src/Items.java
+++ 113-project/src/Items.java
@@ -1,4 +1,17 @@
 
-public class Items {
+public abstract  class Items {
+protected String name;
+protected double price;
+
+public items(String name, int price){
+this.name=name;
+this.price=price;
+}
+
+public abstract double caculatePrice();
+
+public String toString(){
+    return "nameis : " + name + "price is: " + price;
+}
 
 }
