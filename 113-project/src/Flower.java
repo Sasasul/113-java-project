2926ede2ab0e89534cfd0e8444e23e7c2553c1f3
L0 <jana16.kahld@gmail.com>
2024-02-27 14:21:22 +0300

    02

--- 113-project/src/Flower.java
+++ 113-project/src/Flower.java
@@ -4,7 +4,7 @@ private String type;
 private int flowerStock=250;
 
 
-public Flower (String name, int price, String type){   // مدري ذا مفروض يجيه قيم ولا نعين شي ثابت :D
+public Flower (String name, int price, String type){   // مدري ذا مفروض يجيه قيم ولا نعين شي ثابت 
 super(name,price);
 this.type=type;
 }
