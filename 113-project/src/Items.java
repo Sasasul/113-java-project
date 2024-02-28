8c4fa6f21f5202b7a5a85cfb1a7c67a34245cba3
otaib <otaib@Sarah>
2024-02-28 16:29:05 +0300

    adding constructor that takes an Object

--- 113-project/src/Items.java
+++ 113-project/src/Items.java
@@ -4,11 +4,16 @@ public abstract  class Items {
 protected String name;
 protected double price;
 
-
+//Constructors 
 public Items(String name, int price){  
 this.name=name;
 this.price=price;
 }
+public Items(Items i) {
+	name=i.name; 
+	price=i.price;
+}
+
 
 public abstract double caculatePrice();
 
