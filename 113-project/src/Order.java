a114723f02fc69a4adbefc3cd8991a2a7953de6a
otaib <otaib@Sarah>
2024-02-28 17:00:31 +0300

    half Of the method

--- 113-project/src/Order.java
+++ 113-project/src/Order.java
@@ -1,4 +1,39 @@
 
 public class Order {
+	private int numOfItems;
+	private Items[] itemList;
+	
+	public Order() {
+		itemList=new Items[10];
+		numOfItems=0;
+		
+	}
+	
+	public boolean addItem(Items i) {
+		if(numOfItems<itemList.length) {
+			itemList[numOfItems++]=new Items(i);//لماذا!
+		return true;
+		}
+	return false;
+	}
+	
+	public boolean removeItem(String name) {
+		boolean ItemRemoved=false;
+		for(int i=0;i<numOfItems;i++) {
+			if(itemList[i].getName().equals(name)) {
+				itemList[i]=null;
+				ItemRemoved=true;
+			}//end if
+		}//end for
+		return ItemRemoved;
+	}
+	
+	public boolean serchItem(String s) {
+		boolean IsThereChoclateBox=false;
+		for(int i=0;i<numOfItems;i++) {
+			if(itemList[i] instanceof  )
+		}
+	}
+
 
 }
