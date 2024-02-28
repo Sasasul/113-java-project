e8c36aadbb14a6f67b64f7b7a9092e9763ccfdf9
otaib <otaib@Sarah>
2024-02-28 16:37:15 +0300

    adding setters & getters

--- 113-project/src/Items.java
+++ 113-project/src/Items.java
@@ -17,8 +17,23 @@ public Items(Items i) {
 
 public abstract double caculatePrice();
 
+// setters and getters
 public String toString(){
     return "name is : " + name + "price is: " + price;
 }
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+
 
 }
+
