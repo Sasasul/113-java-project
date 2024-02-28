6ca21a59e90f422b4b8dcd4d23ed2d0812697adf
otaib <otaib@Sarah>
2024-02-28 20:23:55 +0300

    completed  ان شاء الله

--- 113-project/src/Customer.java
+++ 113-project/src/Customer.java
@@ -1,7 +1,33 @@
 
 public class Customer {
+
+
 	private String name;
 	private String id;
-	private String test;
 	
+	//Constructor
+	public Customer(String name,String id){
+		this.name=name;
+		this.id=id;
+	}
+	
+	public String toString() {
+		return "Customer Name: "+name+" Customer ID: "+id+"\n";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 }
--- 113-project/src/Order.java
+++ 113-project/src/Order.java
@@ -1,55 +1,65 @@
 
 public class Order {
-	//private numOfOrder;
-	private int numOfItems;
+	private int numOfOrder;
 	private Items[] itemList;
-	
-	public Order() {
-		itemList=new Items[10];
-		numOfItems=0;
-		
+	private int numOfItems;
+	private Customer customer;
+
+	public Order(Customer customer) { // Constructor
+		customer = new Customer(customer.getName(), customer.getId());
+		itemList = new Items[10];
+		numOfItems = 0;
+		numOfOrder++;
 	}
-	
+
+	// Methods
 	public boolean addItem(Items i) {
-		if(numOfItems<itemList.length) {
-			if(i instanceof ChocolateBox)
-				itemList[numOfItems++]=new ChocolateBox((ChocolateBox)i);//لماذا!
+		if (numOfItems < itemList.length) {
+			if (i instanceof ChocolateBox)
+				itemList[numOfItems++] = new ChocolateBox((ChocolateBox) i);
 			else
-				itemList[numOfItems++]=new Flower((Flower)(i.getName(),i.getPrice(),i.getType()));
-		}//end if
-	
+				itemList[numOfItems++] = new Flower((Flower) (i));
+			return true;
+		} // end if
+		else
+			return false;
 	}
-	
+
 	public boolean removeItem(String name) {
-		boolean ItemRemoved=false;
-		for(int i=0;i<numOfItems;i++) {
-			if(itemList[i].getName().equals(name)) {
-				itemList[i]=null;
-				ItemRemoved=true;
-			}//end if
-		}//end for
+		boolean ItemRemoved = false;
+		for (int i = 0; i < numOfItems; i++) {
+			if (itemList[i].getName().equals(name)) {
+				itemList[i] = null;
+				ItemRemoved = true;
+			} // end if
+		} // end for
 		return ItemRemoved;
 	}
-	
+
 	public boolean serchItem(String s) {
-		boolean IsThereChoclateBox=false;
-		for(int i=0;i<numOfItems;i++) {
-			if(itemList[i] instanceof ChocolateBox ) {}
-			IsThereChoclateBox=true;
+		boolean IsThereChoclateBox = false;
+		for (int i = 0; i < numOfItems; i++) {
+			if (itemList[i] instanceof ChocolateBox) {
+			}
+			IsThereChoclateBox = true;
 		}
 		return IsThereChoclateBox;
 	}
 
 	public double calculateTotalprice() {
-		double TotalPrice=0;
-		for(int i=0;i<numOfItems;i++) {
-			TotalPrice+=itemList[i].caculatePrice();
+		double TotalPrice = 0;
+		for (int i = 0; i < numOfItems; i++) {
+			TotalPrice += itemList[i].caculatePrice();
 		}
 		return TotalPrice;
 	}
-	
+
 	public String toString() {
-		String info="";
-	} 
+		String info = "Order Number: " + numOfOrder + "\n" + "Customer info: " + customer.toString();
+		for (int i = 0; i < numOfItems; i++) {
+			info += itemList[i].toString();
+		}
+		return info;
+	}
 
 }
