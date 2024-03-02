be8260915d32005471192be7272b61b56bfce5df
asma1 <asma1@Asma>
2024-03-02 14:33:13 +0300

    Add ord object

--- 113-project/src/FlowerStore.java
+++ 113-project/src/FlowerStore.java
@@ -10,8 +10,9 @@ System.out.println("Welcome to Flower Store, enter your name and id : ");
 String name= input.next();
 String ID = input.next();
 Customer c1= new Customer(name,ID);
-Items obj1= new ChocolateBox("chocolate", 60,'M');
-Items obj2= new Flower("Blossom", 5,1 );
+Order ord= new Order(c1);
+Items it1= new ChocolateBox("chocolate", 60,'M');
+Items it2= new Flower("Blossom", 5,1 );
 do {
 	System.out.println("Enter your menu option:");	
 	System.out.println("1- Add Item");		
@@ -27,26 +28,26 @@ switch(op)
 case 1:
 	System.out.println("Enter item: "); // مو متأكدة منه 
 	
-	c1.addItem(obj1);
-	c1.addItem(obj2);
+	ord.addItem(it1);
+	ord.addItem(it2);
 break;	
 case 2:
 	System.out.println("Enter the item name you want to remove: Rose, Lily,Blossom and ChocolateBox ");
 	String reitem = input.next();
-	 c1.removeItem(reitem);
+	ord.removeItem(reitem);
 	break;	
 case 3:
 	System.out.println("Enter the item name you want to serch: Rose, Lily,Blossom and ChocolateBox ");
 	String seitem = input.next();
-	c1.serchChocolateBox(seitem);
+	ord.serchChocolateBox(seitem);
 	
 	
 	break;	
 case 4:
-	c1.calculateTotalprice();
+	ord.calculateTotalprice();
 	break;	
 case 5:
-	c1.toString()
+	ord.toString()
 	break;	
 
 	
